0dda90a0e625f3aa8c6d041960b882c93c4e4c3d
MiriamZamoraPerez12 <101531398+MiriamZamoraPerez12@users.noreply.github.com>
2022-03-30 23:54:41 -0600

    Add files via upload

---
+++ ESTRUCTURAS CONDICIONALES/CONDICIONES.js
@@ -0,0 +1,33 @@
+var nota = 7;
+console.log("He realizado mi examen. Mi resultado es el siguiente:");
+
+
+switch (nota) {
+  case 10:
+    calificacion = "Sobresaliente";
+    break;
+  case 9:
+  case 8:
+    calificacion = "Notable";
+    break;
+  case 7:
+  case 6:
+    calificacion = "Bien";
+    break;
+  case 5:
+    calificacion = "Suficiente";
+    break;
+  case 4:
+  case 3:
+  case 2:
+  case 1:
+  case 0:
+    calificacion = "Insuficiente";
+    break;
+  default:
+    // Cualquier otro caso
+    calificacion = "Nota errónea";
+    break;
+}
+
+console.log("He obtenido un", calificacion);
